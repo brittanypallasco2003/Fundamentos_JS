34e9ce30a06a92916710184533402ebb8ebf70f1
brittanypallasco2003 <nohemiespinel0@gmail.com>
2023-11-27 01:00:18 -0500

    lab5

---
+++ lab5/dom.js
@@ -0,0 +1,57 @@
+console.log("-----  DOOM Manipulation  -----")
+
+console.log("\n***  Acceder a los elementos HTML  ***")
+
+console.log(document.body)
+
+// console.log("\n***  Obtener clases del elemento  ***")
+
+// var titulo = document.getElementsByClassName("miTitulo")
+
+// console.log("\n***  Seleccionar elementos por clase  ***")
+
+// var elementosConClaseParrafo = document.getElementsByClassName("miClase")
+
+// console.log("\n***  Seleccionar elementos por id  ***")
+
+// var parrafoPorId = document.getElementById("miParrafo")
+
+// console.log("\n***  Seleccionar elementos con querySelector  ***")
+
+// var primerParrafo = document.querySelector("p")
+
+// console.log("\n***  Seleccionar elementos con querySelectorAll  ***")
+
+// var todosLosParrafos = document.querySelectorAll("p")
+
+// console.log("\n***  Modificar el contenido   ***")
+
+// document.querySelector('.tm-heading-large').textContent="ESFOT-EPN"
+
+// console.log("\n***  Modificar el estilo  ***")
+
+// const buscador = document.querySelector('#search-178')
+// buscador.style.border="10px green solid"
+
+// console.log("\n***  Agregar elementos  ***")
+
+// const buscador = document.querySelector('#search-178')
+// const button = document.createElement("button")
+// button.innerText="Hello React"
+// buscador.appendChild(button);
+
+// console.log("\n***  Eliminar elementos  ***")
+
+// const buscador = document.querySelector('#search-178')
+// buscador.remove()
+
+// console.log("\n***  Eventos  ***")
+
+// const buscador = document.querySelector('#search-178')
+// const button = document.createElement("button")
+// button.innerText="Hello React"
+// buscador.appendChild(button)
+
+// button.addEventListener('click',function () {
+//     alert("Abrir Popup")
+// })
